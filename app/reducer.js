1546b32cdd0878aae5c9dce1e059ee4c261acaaa
Evan Sosenko <razorx@evansosenko.com>
2016-08-08 13:40:57 -0700

    Add modules

--- app/reducer.js
+++ app/reducer.js
@@ -4,9 +4,12 @@ import { intlReducer } from 'react-intl-redux'
 import { combineReducers } from 'redux'
 import { router5Reducer } from 'redux-router5'
 
+import * as reducers from './modules'
+
 export const rootReducer = combineReducers({
   intl: intlReducer,
-  router: router5Reducer
+  router: router5Reducer,
+  ...reducers
 })
 
 export default rootReducer
