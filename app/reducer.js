b064b5bbce416da96c49a95fb50751a10c7e08f5
Evan Sosenko <razorx@evansosenko.com>
2016-08-09 00:01:03 -0700

    Rename imported reducers

--- app/reducer.js
+++ app/reducer.js
@@ -1,14 +1,14 @@
 /* @flow */
 
-import { intlReducer } from 'react-intl-redux'
+import { intlReducer as intl } from 'react-intl-redux'
 import { combineReducers } from 'redux'
-import { router5Reducer } from 'redux-router5'
+import { router5Reducer as router } from 'redux-router5'
 
 import * as reducers from './modules'
 
 export const rootReducer = combineReducers({
-  intl: intlReducer,
-  router: router5Reducer,
+  intl,
+  router,
   ...reducers
 })
 
