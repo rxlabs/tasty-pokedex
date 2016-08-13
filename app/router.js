6b2390dbc749f25a46cab265b34417cebc26a008
Evan Sosenko <razorx@evansosenko.com>
2016-08-12 20:45:21 -0700

    Shorten createRouter

--- app/router.js
+++ app/router.js
@@ -6,9 +6,7 @@ import historyPlugin from 'router5-history'
 import routes from './routes'
 
 export const createRouter = () => {
-  const router = new Router5(routes)
+  return new Router5(routes)
     .setOption('useHash', true)
     .usePlugin(historyPlugin())
-
-  return router
 }
